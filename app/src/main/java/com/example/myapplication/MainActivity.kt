fb3d0ac9104f4af78f7281d39fa447fc4bd6cb64
Gamal-prog <124503906+Gamal-prog@users.noreply.github.com>
2024-11-08 14:34:42 +0600

    Updated

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 import androidx.navigation.NavController
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    //Greeting("Android")
+
                     val navController = rememberNavController()
                     NavHost(navController = navController, startDestination = "start_screen") {
                         composable("start_screen") {
@@ -44,14 +44,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//            text = "Hello $name!",
-//            modifier = modifier
-//    )
-//}
 
 @Composable
 fun Start_Screen(navController: NavController) {
@@ -88,12 +80,3 @@ fun Finish_Screen(navController: NavController) {
     }
 
 }}
-
-
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    MyApplicationTheme {
-//        Greeting("Android")
-//    }
-//}}
