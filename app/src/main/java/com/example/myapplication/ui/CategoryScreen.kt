bb9252420bdb9aa38839e56c47450c880f882f72
Gamal-prog <atemil791@gmail.com>
2024-12-17 15:31:40 +0600

    final done

--- app/src/main/java/com/example/myapplication/ui/CategoryScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/CategoryScreen.kt
@@ -3,38 +3,62 @@ package com.example.myapplication.ui
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.network.Category
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.ui.unit.dp
-
-
-
 @Composable
 fun CategoryScreen(
     categories: List<Category>,
     navigateToDetail: (Category) -> Unit
 ) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2), // Устанавливаем 2 столбца
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(8.dp)
-    ) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
         items(categories) { category ->
-            CategoryItem(
-                category = category,
-                navigateToDetail = navigateToDetail
-            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { navigateToDetail(category) }
+                    .padding(16.dp)
+            ) {
+                CategoryItem(category = category)
+            }
         }
     }
 }
 
+
+
+//2 COLUMNS
+//package com.example.myapplication.ui
+//
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.unit.dp
+//import com.example.myapplication.network.Category
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.foundation.lazy.grid.GridCells
+//import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+//import androidx.compose.foundation.lazy.grid.items
+//
+//@Composable
+//fun CategoryScreen(
+//    categories: List<Category>,
+//    navigateToDetail: (Category) -> Unit
+//) {
+//    LazyVerticalGrid(
+//        columns = GridCells.Fixed(2),
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .padding(8.dp)
+//    ) {
+//        items(categories) { category ->
+//            CategoryItem(
+//                category = category,
+//                navigateToDetail = navigateToDetail
+//            )
+//        }
+//    }
+//}
+//
